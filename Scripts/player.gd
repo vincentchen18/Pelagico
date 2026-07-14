572bdf07188ca813b86bffef155afb19b08a0a6f
vincentchen18 <vincent.y.chen2011@gmail.com>
2026-07-14 18:36:40 +1000

    add turn movement

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,19 +1,24 @@
 extends CharacterBody2D
 
 @export var speed := 300.0
-@export var acceleration := 4.0  
-@export var deceleration := 2.0          
+@export var acceleration := 4.0
+@export var drag := 2.0
+@export var turn_speed := 3.0   # radians/sec when turning
 
 @onready var sprite: Node2D = $Sprite2D
 
 func _physics_process(delta):
-	var dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	if dir.x != 0:
-		sprite.flip_h = dir.x > 0
+	# turning
+	var turn = Input.get_axis("ui_left", "ui_right")
+	rotation += turn * turn_speed * delta
 
-	if dir != Vector2.ZERO:
-		velocity = velocity.lerp(dir * speed, acceleration * delta)
+	# fwd/bkwd
+	var thrust = Input.get_axis("ui_down", "ui_up")  # up = forward
+	if thrust != 0:
+		# go in the curr direction
+		var forward = Vector2.RIGHT.rotated(rotation)
+		velocity = velocity.lerp(forward * speed * thrust, acceleration * delta)
 	else:
-		velocity = velocity.lerp(Vector2.ZERO, deceleration * delta)
+		velocity = velocity.lerp(Vector2.ZERO, drag * delta)
 
 	move_and_slide()
