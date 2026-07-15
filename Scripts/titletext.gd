695bc6f54a40646cea72097e178da81d084b3127
yclobb <lucas.liu8621@gmail.com>
2026-07-15 17:07:08 +1000

    added title text anim

---
+++ Scripts/titletext.gd
@@ -0,0 +1,17 @@
+extends RichTextLabel
+@onready var startpos: Vector2 = global_position
+@export var dropdistance: int 
+@export var duration: float
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	global_position.y -= dropdistance
+	_textanim()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _textanim() -> void:
+	var tween: Tween = create_tween()
+	tween.tween_property(self, "position", startpos, duration).set_trans(Tween.TRANS_ELASTIC).set_ease(Tween.EASE_OUT)
