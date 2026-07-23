0f8e2ed8fdc6066193bd1bc3cde4492bd1231f91
vincentchen18 <vincent.y.chen2011@gmail.com>
2026-07-23 20:16:43 +1000

    tune fish animation

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -7,7 +7,7 @@ extends CharacterBody2D
 @export var maxstamina: int = 100
 @export var boostmult: float = 1.4
 @export var animplaymult: float = 1.0
-
+@export var anim_speed := 0.5
 var stamina: float = maxstamina
 @onready var sprite: Node2D = $Sprite2D
 @onready var staminabar: ProgressBar = $ProgressBar
@@ -35,13 +35,13 @@ func _physics_process(delta):
 			velocity = velocity.lerp(forward * speed * thrust, acceleration * delta)
 			boosting = false
 		animplaymult = max(velocity.length()/speed, 1)
-		$AnimatedSprite2D.speed_scale = animplaymult
+		$AnimatedSprite2D.speed_scale = animplaymult * anim_speed
 		$AnimatedSprite2D.play("swim")
 	elif turn != 0:
 		animtimer = 0.15
 		velocity = velocity.lerp(forward * rotforwardspeed, acceleration * delta * 0.6)
 		animplaymult = velocity.length() / speed
-		$AnimatedSprite2D.speed_scale = max(animplaymult, 0.4) 
+		$AnimatedSprite2D.speed_scale = max(animplaymult, 0.4) * anim_speed
 		$AnimatedSprite2D.play("swim")
 		boosting = false
 	else:
@@ -52,7 +52,7 @@ func _physics_process(delta):
 			if animtimer <= 0:
 				$AnimatedSprite2D.stop()
 		else:
-			$AnimatedSprite2D.speed_scale = 0.78
+			$AnimatedSprite2D.speed_scale = 0.78 * anim_speed
 		velocity = velocity.lerp(Vector2.ZERO, drag * delta)
 		boosting = false
 	if not $AnimatedSprite2D.is_playing():
