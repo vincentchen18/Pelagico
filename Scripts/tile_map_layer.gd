accf07acb6ea2b8d996149b4236984f743693b65
vincentchen18 <vincent.y.chen2011@gmail.com>
2026-08-16 10:22:06 +1000

    new emeny sardine spawn in batch

--- Scripts/tile_map_layer.gd
+++ Scripts/tile_map_layer.gd
@@ -1,26 +1,34 @@
 extends TileMapLayer
-
 const sourceid = 0
 var walltilepos = Vector2i(1,2)
 const CRAB = preload("res://enemys/crab.tscn")
-const CRAB_CHANCE = 0.02
-
+const SARDINE = preload("res://enemys/sardine.tscn")
+const CRAB_CHANCE = 0.05
+const SARDINE_CHANCE = 0.06
 var worldmap: Dictionary = {}
 var noise = FastNoiseLite.new()
 var depthnoise = FastNoiseLite.new()
-var chunkvector = Vector2(0, 0)
 var oceantilespos = Vector2i(0, 1)
-var chunkoffset: Vector2 = Vector2.ZERO
-
 @export var player: Node2D
 @export var unload_dist := 4
-
+func _ready() -> void:
+	noise.noise_type = FastNoiseLite.TYPE_SIMPLEX
+	noise.seed = randi()
+	noise.frequency = 0.12
+	depthnoise.noise_type = FastNoiseLite.TYPE_SIMPLEX
+	depthnoise.seed = randi()
+	depthnoise.frequency = 0.03
+func _process(_delta: float) -> void:
+	var playerchunky: Vector2i = playerchunk(player.global_position)
+	for x in range(-2 + playerchunky.x, 3 + playerchunky.x):
+		for y in range(-2 + playerchunky.y, 3 + playerchunky.y):
+			gen_chunk(x, y)
+	unload_far(playerchunky)
 func unload_far(pc: Vector2i) -> void:
 	for key in worldmap.keys():
 		if abs(key.x - pc.x) > unload_dist or abs(key.y - pc.y) > unload_dist:
 			clear_chunk(key)
 			worldmap.erase(key)
-
 func clear_chunk(key: Vector2i) -> void:
 	var minx = key.x * 8 * 32
 	var maxx = minx + 8 * 32
@@ -33,30 +41,21 @@ func clear_chunk(key: Vector2i) -> void:
 	for i in range(8):
 		for j in range(8):
 			erase_cell(Vector2i(key.x * 8 + j, key.y * 8 + i))
-
-func _ready() -> void:
-	noise.noise_type = FastNoiseLite.TYPE_SIMPLEX
-	noise.seed = randi()
-	noise.frequency = 0.12
-	depthnoise.noise_type = FastNoiseLite.TYPE_SIMPLEX
-	depthnoise.seed = randi()
-	depthnoise.frequency = 0.03
-
-func _process(_delta: float) -> void:
-	var playerchunky: Vector2i = playerchunk(player.global_position)
-	for x in range(-2 + playerchunky.x, 3 + playerchunky.x):
-		for y in range(-2 + playerchunky.y, 3 + playerchunky.y):
-			gen_chunk(x, y)
-	unload_far(playerchunky)
-
+func zone_index(globalx: int, globaly: int) -> int:
+	var wobble = depthnoise.get_noise_1d(globaly) * 6.0
+	var wx = globalx + wobble
+	if wx > 352:   return 4
+	elif wx > 160: return 3
+	elif wx > 64:  return 2
+	elif wx > -16: return 1
+	else:          return 0
 func zone_tiles(globalx: int, globaly: int) -> Array:
 	match zone_index(globalx, globaly):
 		4: return [Vector2i(1,1), Vector2i(1,3)] # abyssal
 		3: return [Vector2i(0,3), Vector2i(1,3)] # midnight
-		2: return [Vector2i(0,2), Vector2i(1,2)] #twilight
-		1: return [Vector2i(0,1), Vector2i(1,2)] #shallow
-		_: return [Vector2i(0,0), Vector2i(0,0)] #beach
-
+		2: return [Vector2i(0,2), Vector2i(1,2)] # twilight
+		1: return [Vector2i(0,1), Vector2i(1,2)] # shallow
+		_: return [Vector2i(0,0), Vector2i(0,0)] # beach
 func gen_chunk(cx, cy) -> void:
 	var chunkkey = Vector2i(cx, cy)
 	var tilepos: Vector2i = Vector2i.ZERO
@@ -83,44 +82,36 @@ func gen_chunk(cx, cy) -> void:
 				else:
 					currentlis.append("o")
 					tilepos = oceantilespos
-					if noise.get_noise_2d(globalx, globaly + 1) > 0.45 and randf() < CRAB_CHANCE:
-						spawn_crab(Vector2i(globalx, globaly))
+					if noise.get_noise_2d(globalx, globaly + 1) > 0.45:
+						spawn_for_zone(Vector2i(globalx, globaly), globalx, globaly)
 			set_cell(Vector2i(globalx, globaly), sourceid, tilepos, 0)
 		currentchunk.append(currentlis)
 	worldmap[chunkkey] = currentchunk
-
-func spawn_crab(cell: Vector2i) -> void:
-	var inst = CRAB.instantiate()
-	get_parent().add_child(inst)
-	inst.global_position = to_global(map_to_local(cell))
-
-func zone_index(globalx: int, globaly: int) -> int:
-	var wobble = depthnoise.get_noise_1d(globaly) * 6.0
-	var wx = globalx + wobble
-	if wx > 352:   return 4
-	elif wx > 160: return 3
-	elif wx > 64:  return 2
-	elif wx > -16: return 1
-	else:          return 0
-
 func spawn_for_zone(cell: Vector2i, globalx: int, globaly: int) -> void:
 	var z = zone_index(globalx, globaly)
-	var r = randf()
 	match z:
-		0, 1:
-			if r < CRAB_CHANCE: spawn(CRAB, cell)
+		0:
+			if randf() < CRAB_CHANCE: spawn(CRAB, cell)
+		1:
+			if randf() < CRAB_CHANCE: spawn(CRAB, cell)
+			if randf() < SARDINE_CHANCE: spawn_school(SARDINE, cell)
 		2:
-			if r < 0.015: pass   #more mobs later
+			pass
 		3, 4:
-			if r < 0.02: pass  
-
+			pass
 func spawn(scene: PackedScene, cell: Vector2i) -> void:
 	var inst = scene.instantiate()
 	get_parent().add_child(inst)
 	inst.global_position = to_global(map_to_local(cell))
-
 func playerchunk(playerpos) -> Vector2i:
 	var pixelsperchunk: float = 256
 	var playerchunkx: int = int(floor(playerpos.x / pixelsperchunk))
 	var playerchunky: int = int(floor(playerpos.y / pixelsperchunk))
 	return Vector2i(playerchunkx, playerchunky)
+func spawn_school(scene: PackedScene, cell: Vector2i, min_count := 5, max_count := 8, spread := 40.0) -> void:
+	var count = randi_range(min_count, max_count)
+	var center = to_global(map_to_local(cell))
+	for i in count:
+		var inst = scene.instantiate()
+		get_parent().add_child(inst)
+		inst.global_position = center + Vector2(randf_range(-spread, spread), randf_range(-spread, spread))
---
+++ enemys/sardinehealth.gd
@@ -0,0 +1,33 @@
+extends ProgressBar
+
+var max_health := 40.0
+var health := max_health
+
+func take_damage(amount):
+	health = clamp(health-amount, 0, max_health)
+	update_bar()
+	
+func heal(amount):
+	health = clamp(health+amount, 0, max_health)
+	update_bar()
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	top_level = true
+	var fill := StyleBoxFlat.new()
+	fill.bg_color = Color(0.2, 0.8, 0.3) 
+	add_theme_stylebox_override("fill", fill)
+
+	var bg := StyleBoxFlat.new()
+	bg.bg_color = Color(0.1, 0.1, 0.1)   
+	add_theme_stylebox_override("background", bg)
+	update_bar()
+func update_bar():
+	value = health
+	var t := float(health) / max_health
+	var fill := StyleBoxFlat.new()
+	fill.bg_color = Color(1.0 - t, t, 0.2)  # make red->green as health go up
+	add_theme_stylebox_override("fill", fill)
+
+func _physics_process(_delta: float) -> void:
+	position = get_parent().get_node("AnimatedSprite2D").global_position + Vector2(-13, 20)
